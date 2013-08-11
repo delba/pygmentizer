badb026298898e9b16643f32dcd475e488534722
delba <delba@server.fake>
2013-08-11 20:51:10 +0200

    snippet controller test

--- test/controllers/snippets_controller_test.rb
+++ test/controllers/snippets_controller_test.rb
@@ -10,7 +10,7 @@ class SnippetsControllerTest < ActionController::TestCase
 
   test "post :create with valid params" do
     assert_difference 'Snippet.count' do
-      post :create, snippet: {
+      xhr :post, :create, snippet: {
         filename: 'person.rb',
         language: 'Ruby',
         content: 'class Person; end'
@@ -18,17 +18,25 @@ class SnippetsControllerTest < ActionController::TestCase
     end
 
     assert assigns(:snippet).persisted?
-    assert_redirected_to root_url
+    assert_template partial: '_snippet'
   end
 
   test "post :create without valid params" do
-    assert_no_difference 'Snippet.count' do
-      post :create, snippet: {
+    assert_raise ActiveRecord::RecordInvalid do
+      xhr :post, :create, snippet: {
         filename: 'noname.rb'
       }
     end
+  end
 
-    assert assigns(:snippet).new_record?
-    assert_template :index
+  test "get :lexers with a valid extension" do
+    get :lexers, filename: 'hello.rb'
+
+    assert_equal 'Ruby', JSON.parse(@response.body)['language']
+  end
+
+  test "get :lexers without a valid extension" do
+    get :lexers, filename: 'hello.noname'
+    assert_empty @response.body
   end
 end
