dbb3e14ef21435b7c4c4e9aecb71065a48c9c8c6
delba <delba@server.fake>
2013-08-16 12:17:19 +0200

    remove extra code tag

--- app/helpers/snippets_helper.rb
+++ app/helpers/snippets_helper.rb
@@ -1,9 +1,9 @@
 module SnippetsHelper
   def pygment_for(snippet)
     if snippet.pygment?
-      content_tag :code, snippet.pygment.html_safe
+      snippet.pygment.html_safe
     else
-      content_tag :code, content_tag(:pre, snippet.content)
+      content_tag :pre, snippet.content
     end
   end
 end
