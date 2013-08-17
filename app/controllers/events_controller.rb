dc6ed4285cdb1641643e75c5d8c7a06d7cd61a45
delba <delba@server.fake>
2013-08-17 13:32:38 +0200

    first draft

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,8 +4,22 @@ class EventsController < ApplicationController
   def highlight
     response.headers['Content-Type'] = 'text/event-stream'
     $redis.subscribe 'highlight' do |on|
-      on.message do |event, data|
-        response.stream.write "data: #{data}\n\n"
+      on.message do |channel, msg|
+        response.stream.write "data: #{msg}\n\n"
+      end
+    end
+  rescue IOError
+    logger.info "Stream Closed"
+  ensure
+    response.stream.close
+  end
+
+  def change
+    response.headers['Content-Type'] = 'text/event-stream'
+    $redis.psubscribe 'change:*' do |on|
+      on.message do |pattern, channel, msg|
+        response.stream.write "event: #{pattern}\n"
+        response.stream.write "data: #{msg}\n\n"
       end
     end
   rescue IOError
--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -6,4 +6,14 @@ class Snippet < ActiveRecord::Base
   validates :language, inclusion: { in: LANGUAGES }
 
   default_scope { order(created_at: :desc) }
+
+  after_update if: :pygment_changed? do
+    $redis.publish 'highlight', self.to_json
+  end
+
+  before_update do
+    self.changed.each do |attribute|
+      $redis.publish "change:#{attribute}", self.to_json
+    end
+  end
 end
